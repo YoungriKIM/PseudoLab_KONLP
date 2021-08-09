076ebc4ffa563f0acf835a84ddfd8f4b19402dda
YoungriKIM <lemontleo0311@gmail.com>
2021-08-09 23:38:52 +0900

    -

--- memo.py
+++ memo.py
@@ -1,7 +1,12 @@
-    process-wiki)
-        echo "processing ko-wikipedia..."
-        mkdir -p /notebooks/embedding/data/processed
-        python preprocess/dump.py --preprocess_mode wiki \
-            --input_path /notebooks/embedding/data/raw/kowiki-latest-pages-articles.xml.bz2 \
-            --output_path /notebooks/embedding/data/processed/processed_wiki_ko.txt
-        ;;
+    # process-wiki)
+    #     echo "processing ko-wikipedia..."
+    #     mkdir -p /notebooks/embedding/data/processed
+    #     python preprocess/dump.py --preprocess_mode wiki \
+    #         --input_path /notebooks/embedding/data/raw/kowiki-latest-pages-articles.xml.bz2 \
+    #         --output_path /notebooks/embedding/data/processed/processed_wiki_ko.txt
+
+
+    # dump-raw-wiki)
+    #     echo "download ko-wikipedia..."
+    #     wget https://dumps.wikimedia.org/kowiki/latest/kowiki-latest-pages-articles.xml.bz2 -P /notebooks/embedding/data/raw
+    #     mkdir -p /notebooks/embedding/data/processed
